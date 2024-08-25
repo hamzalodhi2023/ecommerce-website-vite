61eeb51f8aabb39ed0ca8cde7004812a38a9e624
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2024-08-25 15:01:59 +0500

    Trusted section has completed

--- src/components/Trusted.jsx
+++ src/components/Trusted.jsx
@@ -1,5 +1,38 @@
 function Trusted() {
-  return <div>Trusted</div>;
+  return (
+    <div className="flex min-h-[50dvh] w-full flex-col items-center justify-evenly bg-[#dee1e4] px-20">
+      <p className="text-center text-xl font-semibold text-[#a1a1a1]">
+        Trusted By 1000+ Companies
+      </p>
+      <div className="flex w-full items-center justify-evenly">
+        <img
+          src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image2.png"
+          alt="trusted-brands"
+          className="aspect-[2/1.5] w-40"
+        />
+        <img
+          src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image3.png"
+          alt="trusted-brands"
+          className="aspect-[2/1.5] w-40"
+        />
+        <img
+          src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image4.png"
+          alt="trusted-brands"
+          className="aspect-[2/1.5] w-40"
+        />
+        <img
+          src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image6.png"
+          alt="trusted-brands"
+          className="aspect-[2/1.5] w-40"
+        />
+        <img
+          src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image8.png"
+          alt="trusted-brands"
+          className="aspect-[2/1.5] w-40"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Trusted;
