877b7b76ac9a5b879063d2bbd60b1188d42c459b
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2024-09-21 16:56:21 +0500

    working on singleproduct page

--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -4,6 +4,9 @@ import { FaRegStarHalfStroke } from "react-icons/fa6";
 import { FaStar } from "react-icons/fa6";
 
 function Stars({ stars, reviews }) {
+  const ratingStars = Array.from({ length: 5 }, (_, index) => {
+    let number = index + 0.5;
+  });
   return (
     <span>
       <span className="font-bold">Rating: </span>
