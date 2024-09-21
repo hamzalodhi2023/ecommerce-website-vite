2f94d5edeceefba7a8b0f060dfc7044433dc21bb
Hamza Khan Lodhi <hamzalodhi2023@gmail.com>
2024-09-21 21:11:18 +0500

    working on singleproduct page

--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -6,14 +6,24 @@ import { FaStar } from "react-icons/fa6";
 function Stars({ stars, reviews }) {
   const ratingStars = Array.from({ length: 5 }, (_, index) => {
     let number = index + 0.5;
+    return (
+      <span key={index}>
+        {stars >= index + 1 ? (
+          <FaStar className="text-yellow-500" />
+        ) : stars >= number ? (
+          <FaRegStarHalfStroke className="text-yellow-500" />
+        ) : (
+          <FaRegStar className="text-yellow-500" />
+        )}
+      </span>
+    );
   });
+
   return (
-    <span>
-      <span className="font-bold">Rating: </span>
-      {stars} Stars ({reviews} Reviews)
-      <FaRegStar />
-      <FaRegStarHalfStroke />
-      <FaStar />
+    <span className="flex items-center">
+      <span className="font-bold">Rating: &nbsp;</span>
+      <div className="flex items-center">{ratingStars}</div>&nbsp;({reviews}{" "}
+      Customer Reviews)
     </span>
   );
 }
